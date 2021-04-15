e9f150d585d7487070f5b7a35f4da595b94761c4
Nicos <hfnovember@hotmail.com>
2021-04-15 11:14:32 +0300

    docs update

--- Source/src/main/java/com/raylabz/javahttp/BinaryHTTPResponse.java
+++ Source/src/main/java/com/raylabz/javahttp/BinaryHTTPResponse.java
@@ -1,7 +1,5 @@
 package com.raylabz.javahttp;
 
-import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
-
 import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.IOException;
